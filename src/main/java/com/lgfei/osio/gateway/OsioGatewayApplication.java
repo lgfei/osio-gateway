18bf41f0e516206c4176e4187d0ebd2a0425f5a2
long.gf <long.gf@topsports.com.cn>
2023-05-30 09:00:06 +0800

    init by https://start.aliyun.com/

---
+++ src/main/java/com/lgfei/osio/gateway/OsioGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.lgfei.osio.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsioGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OsioGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lgfei/osio/gateway/OsioGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lgfei.osio.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsioGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
